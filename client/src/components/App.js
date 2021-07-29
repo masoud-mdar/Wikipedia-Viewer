436dbd30029ecda4a4c0383552196acd8a14f770
Masoud <masoud_mdar@yahoo.fr>
2021-07-29 17:24:19 +0200

    event listener, styles for input and close btn

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,38 +3,37 @@ import axios from "axios"
 
 import BASE_URL from "../constants"
 
+// to remove event listener !!!!!!!!!!!!!!!
 
 const App = () => {
 
     const [lang, setLang] = useState("en")
     const [searchInput, setSearchInput] = useState("")
     const [results, setResults] = useState([])
-    const [displayResult, setDisplayResult] = useState("none")
+    
     const [isSearchActive, setIsSearchActive] = useState(false)
+
+    const [displayResult, setDisplayResult] = useState("none")
     const [displayLine, setDisplayLine] = useState("block")
+    const [displayParag, setDisplayParag] = useState("block")
 
 
     const langsArr = ["en", "fr", "de", "fa", "es", "oc", "ar"]
 
-    const handleChange = (Event) => {
-        const {name, value} = Event.target
+    const activeSearchStyle = {width: "17vw", height: "5vh", borderRadius: "25px"}
 
-        if (name === "search") {
-            setSearchInput(value)
-        }
+    useEffect(()=> {
+        document.addEventListener("keydown", handleKeyPress)
 
-    }
+    })
 
-    const handleClick = (Event) => {
-        const {name, innerHTML} = Event.target
-
-        if (name === "lang-select") {
-            setLang(innerHTML)
-
-        } else if (name === "go") {
-
+    const handleKeyPress = (Event) => {
+        console.log(Event.keyCode)
+        if (isSearchActive && searchInput && Event.keyCode === 13) {
+            
             //const url = `https://${lang}.wikipedia.org/w/api.php`
             setDisplayResult("block")
+            setDisplayParag("none")
 
             let sendingData = [
                 lang,
@@ -59,9 +58,37 @@ const App = () => {
                 setResults(data.query.search)
 
             })
+        }
+    }
+
+    const handleChange = (Event) => {
+        const {name, value} = Event.target
+
+        if (name === "search") {
+            setSearchInput(value)
+        }
+
+    }
+
+    const handleClick = (Event) => {
+        let {name, innerHTML} = Event.target
+
+
+        if (name === "lang-select") {
+            setLang(innerHTML)
+
+        } else if (name === "go") {
+            // moved it to handleKeyPress
         } else if (name === "search") {
             setIsSearchActive(true)
             setDisplayLine("none")
+        } else if (name === "close") {
+            setIsSearchActive(false)
+            setDisplayLine("block")
+            setSearchInput("")
+            setResults([])
+            setDisplayResult("none")
+            setDisplayParag("block")
         }
     }
 
@@ -89,11 +116,16 @@ const App = () => {
 
                     <div className="search-wrapper">
                         <div className="input-wrapper">
-                            <input name="search" onChange={handleChange} onClick={handleClick} value={searchInput} placeholder=""></input>
+                            <input name="search" 
+                            onChange={handleChange} onClick={handleClick} 
+                            value={searchInput} placeholder=""
+                            style={isSearchActive ? activeSearchStyle : {}}
+                            ></input>
                             <div className="line" style={{display: displayLine}}></div>
-                            <button name="go" onClick={handleClick}>click here</button>
+                            {/*<button name="go" onClick={handleClick}>click here</button>*/}
+                            <button name="close" onClick={handleClick} className="close-btn" style={isSearchActive ? {display: "block"}: {display: "none"}}>X</button>
                         </div>
-                        <p>Click icon to search</p>
+                        <p style={{display: displayParag}}>Click icon to search</p>
                     </div>
                 </div>
 
